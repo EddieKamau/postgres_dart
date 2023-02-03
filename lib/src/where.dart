fe95e7f1de1c199bab3abe306fcc0a2a18415922
Eddie Genius <elyudde@gmail.com>
2023-02-03 14:06:17 +0300

    where selector

---
+++ lib/src/where.dart
@@ -0,0 +1,63 @@
+class Where {
+  Where(
+    this.columnName,
+    this.operator,
+    this.value,
+    
+  );
+  Where.not(
+    this.columnName,
+    this.operator,
+    this.value,
+    
+  ){
+    prev = 'WHERE NOT ';
+  }
+
+  final String columnName;
+  final Object value;
+  final WhereOperator operator;
+  String prev = 'WHERE';
+
+  String get query {
+    // ignore: no_leading_underscores_for_local_identifiers
+    Object _value = value is String ? "'$value'" : value;
+    return '$prev "$columnName" ${operator.operator} $_value ';
+  }
+
+  Where or(String columnName, WhereOperator operator, Object value, ){
+    var where = Where(columnName, operator, value);
+    where.prev = '$query OR ';
+    return where;
+  }
+
+  Where and(String columnName, WhereOperator operator, Object value, ){
+    var where = Where(columnName, operator, value);
+    where.prev = '$query AND ';
+    return where;
+  }
+
+  Where andNot(String columnName, WhereOperator operator, Object value, ){
+    var where = Where(columnName, operator, value);
+    where.prev = '$query AND NOT ';
+    return where;
+  }
+
+}
+
+enum WhereOperator{
+  isEqual('='),
+  isGreaterThan('>'),
+  isLessThan('<'),
+  isGreaterThanOrEqual('>='),
+  isLessThanOrEqual('<='),
+  isNotEqual('<>'),
+  isBetween('BETWEEN'),
+  isLike('LIKE'),
+  isIn('IN'),
+  isNull('IS NULL'),
+  isNotNull('IS NOT NULL');
+
+  final String operator;
+  const WhereOperator(this.operator);
+}
